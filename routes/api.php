e86257b24fd9b0a28e9991c27de429357fe2bca5
luisrances <francisluis.rances@gmail.com>
2025-12-31 00:02:05 +0800

    feat(device-backend): implement complete backend for devices table with industry standards (#4)

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\UserController;
 use App\Http\Controllers\Api\V1\CustomerController;
+use App\Http\Controllers\Api\V1\DeviceController;
 
 // API Version 1 Routes
 Route::prefix('v1')->group(function () {
@@ -14,4 +15,8 @@ Route::prefix('v1')->group(function () {
     // Customer resource routes
     Route::apiResource('customers', CustomerController::class)->except(['destroy']);
     Route::delete('customers/{customer}', [CustomerController::class, 'destroy']);
+
+    // Device resource routes
+    Route::apiResource('devices', DeviceController::class)->except(['destroy']);
+    Route::delete('devices/{device}', [DeviceController::class, 'destroy']);
 });
